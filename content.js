1cd74bc935fc348e246c753716c153105925a37e
Nikita Ramoji <nikita_ramoji@brown.edu>
2017-02-04 16:07:32 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 var elements = document.getElementsByTagName('*');
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-   chrome.tabs.executeScript(null, {file: "testScript.js"});
+   console.log("The button was clicked.");
 });
 
 
