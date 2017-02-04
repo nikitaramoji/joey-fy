453f79258687a5517300669b33c362a2c324c4df
Nikita Ramoji <nikita_ramoji@brown.edu>
2017-02-04 18:22:54 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,31 @@
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-// Called when the user clicks on the browser action.
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) { 
-   chrome.tabs.executeScript(null, {file: "content.js"});
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Making the page joeyfied!');
+  chrome.tabs.executeScript({
+     var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/heart/g, 'aortic pump');
+            replacedText = replacedText.replace(/Heart/g, 'Aortic pump');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
+     
+  });
 });
