2b9865c9a00b6efacaf74db22be707074f9eb94c
Nikita Ramoji <nikita_ramoji@brown.edu>
2017-02-04 14:32:24 -0500

    Updated content.js to account for capitalization

--- content.js
+++ content.js
@@ -8,7 +8,8 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/heart/gi, 'aortic pumps');
+            var replacedText = text.replace(/heart/g, 'aortic pump');
+            replacedText = replacedText.replace(/Heart/g, 'Aortic pump');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
