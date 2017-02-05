13477742db205f69a8b16c705e0ddc8e590f2117
Nikita Ramoji <nikita_ramoji@brown.edu>
2017-02-04 21:53:17 -0500

    Update content.js

--- content.js
+++ content.js
@@ -24,13 +24,14 @@ const rp = require ("request-promise");
 //input word is the word that the program will search for
 function getSynonyms (word) {
   rp ("https://words.bighugelabs.com/api/2/9711165e9d159d59f71d4a0c983efe37/" + word + "/json")
+  var synonyms = null;
   .then(function(data){
     var nouns = JSON.parse(data)['noun']['syn'];
     var verbs = JSON.parse(data)['verb']['syn'];
-    var synonyms = nouns.concat(verbs);
+    synonyms = nouns.concat(verbs);
     console.log(synonyms);
-    return synonyms;
   });
+  return synonyms;
 }
 
 //longestWord in a list
